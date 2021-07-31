cad3bceac4901d669b6e1d54f298be3584c8870d
Arghya Chanda <arghyac35@gmail.com>
2021-07-31 23:55:00 +0530

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+require('jest-preset-angular/ngcc-jest-processor');
+module.exports = {
+  moduleNameMapper: {
+    '@core/(.*)': '<rootDir>/app/core/$1',
+    '@shared/(.*)': '<rootDir>/app/shared/$1',
+  },
+  rootDir: 'src',
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['../setup-jest.ts'],
+  collectCoverage: true,
+  coverageReporters: ['lcov'],
+  coverageDirectory: '../coverage/jest',
+  collectCoverageFrom: ['**/*.ts'],
+};
