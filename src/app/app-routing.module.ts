d73972f33038572e6359d16f5e0b3849e11ece13
Arghya Chanda <arghyac35@gmail.com>
2021-07-31 18:33:45 +0530

    Cleanup
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  // Redirect empty path to '/example'
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+
+  // Redirect signed in user to the '/home'
+  //
+  // After the user signs in, the sign in page will redirect the user to the 'signed-in-redirect'
+  // path. Below is another redirection for that path to redirect the user to the desired
+  // location. This is a small convenience to keep all main routes together here on this file.
+  { path: 'signed-in-redirect', pathMatch: 'full', redirectTo: 'home' },
+
+  // Auth routes for guests
   {
     path: '',
-    loadChildren: () => import('./features/after-login/after-login.module').then((m) => m.AfterLoginModule),
-    // change to before if session resume is not supported in your app
+    // canActivate: [NoAuthGuard],
+    // canActivateChild: [NoAuthGuard],
+    component: AppComponent,
+    children: [
+      // { path: 'sign-in', loadChildren: () => import('app/modules/auth/sign-in/sign-in.module').then(m => m.AuthSignInModule) },
+      // { path: 'sign-up', loadChildren: () => import('app/modules/auth/sign-up/sign-up.module').then(m => m.AuthSignUpModule) }
+    ],
   },
+
+  // Admin routes
   {
-    path: 'after-login',
-    loadChildren: () => import('./features/after-login/after-login.module').then((m) => m.AfterLoginModule),
-  },
-  {
-    path: 'before-login',
-    loadChildren: () => import('./features/before-login/before-login.module').then((m) => m.BeforeLoginModule),
-  },
-  {
-    path: '**',
-    redirectTo: 'after-login', // or 404 module
+    path: '',
+    // canActivate: [AuthGuard],
+    // canActivateChild: [AuthGuard],
+    component: AppComponent,
+    children: [
+      { path: 'home', loadChildren: () => import('./features/example/example.module').then((m) => m.ExampleModule) },
+    ],
   },
 ];
 
